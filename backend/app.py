bcf2a85cf30041d5f71c03e7c918abb9285d52f1
wenmu <21371243@buaa.edu.cn>
2023-12-10 09:13:16 +0800

    backend

---
+++ backend/app.py
@@ -0,0 +1,197 @@
+from flask import Flask, render_template, url_for, jsonify, request
+from flask_cors import CORS
+import pymysql
+from werkzeug.security import generate_password_hash, check_password_hash
+
+pymysql.install_as_MySQLdb()
+
+app = Flask(__name__)
+CORS(app)
+# 配置数据库连接信息，例如数据库的URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:lxj021105@localhost:3306/buaa'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+from model import *
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+
+    user_name = data.get('user_name')
+    user_key = data.get('user_key')
+
+    if not user_name or not user_key:
+        return jsonify({'message': 'Missing username or password'}), 400
+
+    # 检查用户名是否已存在
+    existing_user = User.query.filter_by(user_name=user_name).first()
+    if existing_user:
+        return jsonify({'message': 'Username already exists. Choose a different username.'}), 409
+
+    hashed_password = generate_password_hash(user_key, method='sha256')
+
+    new_user = User(user_name=user_name, user_key=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'User registered successfully!'}), 201
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    try:
+        data = request.get_json()
+
+        user_name = data.get('user_name')
+        user_key = data.get('user_key')
+        if not user_name or not user_key:
+            return jsonify({'message': 'Missing username or password'}), 400
+
+        user = User.query.filter_by(user_name=user_name).first()
+
+        if not user or not check_password_hash(user.user_key, user_key):
+            return jsonify({'message': 'Invalid username or password'}), 401
+
+        # You can generate a token here if you want to implement token-based authentication
+
+        return jsonify({'message': 'Login successful!'}), 200
+    except Exception as e:
+        return jsonify({'message': str(e)}), 500
+
+
+@app.route('/modify_password', methods=['POST'])
+def modify_password():
+    data = request.get_json()
+    user = User.query.filter_by(user_name=data['user_name']).first()
+
+    if user and check_password_hash(user.user_key, data['old_password']):
+        user.user_key = generate_password_hash(data['new_password'], method='sha256')
+        db.session.commit()
+        return jsonify({'message': '密码修改成功'}), 200
+    else:
+        return jsonify({'message': '无效的凭证或旧密码'}), 401
+
+
+# 注销用户
+@app.route('/logout/<string:user_name>', methods=['POST'])
+def logout(user_name):
+    user = User.query.filter_by(user_name=user_name).first()
+
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify({'message': 'User logged out successfully!'}), 200
+
+
+@app.route('/create_post', methods=['POST'])
+def create_post():
+    try:
+        # 从请求中获取数据
+        user_id = request.json.get('user_id')
+        content = request.json.get('content')
+        # 创建新的帖子并添加到数据库
+        user = User.query.get(user_id)
+        if user:
+            new_post = Post(content=content, u_id=user_id)
+            db.session.add(new_post)
+            db.session.commit()
+            return jsonify({'message': 'Post created successfully!'}), 201
+        else:
+            return jsonify({'error': 'User not found!'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/show_posts', methods=['GET'])
+def get_posts():
+    posts = Post.query.all()
+    post_list = []
+
+    for post in posts:
+        post_info = {
+            'post_id': post.post_id,
+            'content': post.content,
+            'user_id': post.user_id
+            # Add more fields as needed
+        }
+        post_list.append(post_info)
+
+    print(jsonify({'posts': post_list}))
+    return jsonify({'posts': post_list})
+
+
+# API endpoint for deleting a post
+@app.route('/delete_post/<int:post_id>', methods=['POST'])
+def delete_post(post_id):
+    try:
+        # Get the post by post_id
+        post_to_delete = Post.query.get(post_id)
+
+        # Check if the logged-in user is the owner of the post
+        logged_in_user_id = request.headers.get('user_id')
+
+        if post_to_delete.user_id == logged_in_user_id:
+            # Delete the post if the user is the owner
+            db.session.delete(post_to_delete)
+            db.session.commit()
+            return jsonify({'message': 'Post deleted successfully'}), 200
+        else:
+            # Return an error if the user is not the owner
+            return jsonify({'error': 'You are not authorized to delete this post'}), 403
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/post_comment', methods=['POST'])
+def post_comment():
+    try:
+        data = request.get_json()
+        content = data['content']
+        from_user_id = data['from_user_id']
+        user = User.query.get(from_user_id)
+        to_post_id = data['to_post_id']
+        post = Post.query.get(to_post_id)
+
+        if user and post:
+            # Insert new comment into the 'comments' table
+            new_comment = Comment(content=content, from_user_id=from_user_id, to_post_id=to_post_id)
+            db.session.add(new_comment)
+            db.session.commit()
+
+            return jsonify({'message': 'Comment posted successfully'}), 201
+        else:
+            return jsonify({'message': '用户或帖子不存在'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+# API endpoint for deleting a comment
+@app.route('/delete_comment/<int:comment_id>', methods=['DELETE'])
+def delete_comment(comment_id):
+    try:
+        # Get the comment by comment_id
+        comment_to_delete = Comment.query.get(comment_id)
+
+        # Check if the logged-in user is the owner of the post
+        logged_in_user_id = request.headers.get('user_id')
+
+        if comment_to_delete.user_id == logged_in_user_id:
+            # Delete the post if the user is the owner
+            db.session.delete(comment_to_delete)
+            db.session.commit()
+
+            return jsonify({'message': 'Comment deleted successfully'}), 200
+        else:
+            # Return an error if the user is not the owner
+            return jsonify({'error': 'You are not authorized to delete this post'}), 403
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
---
+++ backend/model.py
@@ -0,0 +1,34 @@
+from app import app
+from flask_sqlalchemy import SQLAlchemy
+
+# 初始化数据库
+db = SQLAlchemy(app)
+
+
+# 定义用户表模型
+class User(db.Model):
+    __tablename__ = 'users'
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_name = db.Column(db.String(20), nullable=False, unique=True)
+    user_key = db.Column(db.Text, nullable=False)
+
+
+# 定义帖子表模型
+class Post(db.Model):
+    __tablename__ = 'posts'
+    post_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    content = db.Column(db.String(1000), nullable=False)
+    u_id = db.Column(db.Integer, db.ForeignKey('users.user_id', ondelete='CASCADE'), nullable=False)
+    user = db.relationship('User', backref=db.backref('posts', cascade='all, delete-orphan'))
+
+
+# 定义评论表模型
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    comment_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    content = db.Column(db.String(1000), nullable=False)
+    from_id = db.Column(db.Integer, db.ForeignKey('users.user_id', ondelete='CASCADE'), nullable=False)
+    to_id = db.Column(db.Integer, db.ForeignKey('posts.post_id', ondelete='CASCADE'), nullable=False)
+    user = db.relationship('User', backref=db.backref('comments', cascade='all, delete-orphan'))
+    post = db.relationship('Post', backref=db.backref('comments', cascade='all, delete-orphan'))
+
